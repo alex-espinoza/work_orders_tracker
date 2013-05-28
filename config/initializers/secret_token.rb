f7b86a57218bb81037a9989acdb17154aaf12aa7
Alex Espinoza <aespinoza@aspria.net>
2013-05-28 17:16:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkOrdersTracker::Application.config.secret_token = '58b49e82a7746fabd4777fdf0dfab6b036472ee99329ab6a63d64e52e54520ad139e8ae57a98e250edc5c0a0bb7107c58c90a11218c95409ba338957bf630192'
