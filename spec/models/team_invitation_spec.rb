117afc2ba32b34f943a183856e0809f446bf9882
Alex Espinoza <aespinoza@aspria.net>
2013-06-27 14:55:58 -0400

    Team invitations feature implemented and tested

--- spec/models/team_invitation_spec.rb
+++ spec/models/team_invitation_spec.rb
@@ -1,20 +1,31 @@
 require 'spec_helper'
 
 describe TeamInvitation do
-	# Anything regarding recipient_email fails because of the self_invitation_check method in TeamInvitation, not sure why
+	let!(:team) { FactoryGirl.create(:invitations_test_team) }
+	let!(:user) { FactoryGirl.create(:new_user) }
+	let!(:membership) { FactoryGirl.create(:invitations_test_membership, user: user, team: team, role: "worker") }
 
   it { should belong_to(:team) }
   it { should belong_to(:sender) }
- #  it { should have_one(:recipient) }
 
- #  it { should validate_presence_of(:recipient_email) }
- #  it { should validate_presence_of(:sender_id) }
- #  it { should validate_presence_of(:team_id) }
+  it "ensure recipient_already_registered? method works correctly." do
+  	team_invitation = TeamInvitation.new
+  	team_invitation.recipient_already_registered?("johndoe@test.com", team)
+  	expect(team.team_memberships.all.count).to eq(2)
+  end
 
-	# it { should validate_presence_of(:recipient_email) }
-	# it { should_not allow_value("blah").for(:recipient_email) }
-	# it { should allow_value("blah@blah.com").for(:recipient_email) }
-	# it { should ensure_length_of(:recipient_email).is_at_least(6).is_at_most(100) }
+  describe "if valid" do
+  	before { subject.stub(:self_invitation_check) { false } }
+  	before { subject.stub(:recipient_already_in_team) { false } }
 
-	# it { should validate_uniqueness_of(:sender_id).scoped_to(:recipient_email, :team_id) }
+  	it { should validate_presence_of(:recipient_email) }
+	  it { should validate_presence_of(:sender_id) }
+	  it { should validate_presence_of(:team_id) }
+
+		it { should_not allow_value("blah").for(:recipient_email) }
+		it { should allow_value("blah@blah.com").for(:recipient_email) }
+		it { should ensure_length_of(:recipient_email).is_at_least(6).is_at_most(100) }
+
+		# it { should validate_uniqueness_of(:sender_id).scoped_to(:recipient_email, :team_id) }
+	end
 end
--- spec/models/team_membership_spec.rb
+++ spec/models/team_membership_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe TeamMembership do
 	let!(:team) { FactoryGirl.create(:invitations_test_team) }
 	let!(:user) { FactoryGirl.create(:invited_user) }
+	let!(:manager) { FactoryGirl.create(:inviter_manager) }
+	let!(:invitation) { FactoryGirl.create(:test_invitation, team: team, sender: manager, recipient_email: user.email) }
 
 	it { should belong_to(:user) }
 	it { should belong_to(:team) }
@@ -11,9 +13,16 @@ describe TeamMembership do
 	it { should validate_presence_of(:user_id) }
 	it { should validate_presence_of(:team_id) }
 
-	it "ensure create_manager_membership method is valid." do
+	it "ensure create_manager_membership method works correctly." do
 		team_membership = TeamMembership.new
 		team_membership.create_manager_membership(user, team)
+		expect(TeamMembership.all.count).to eq(1)
+	end
+
+	it "ensure create_worker_membership method is works correctly." do
+		team_membership = TeamMembership.new
+		user.invitation = invitation
+		team_membership.create_worker_membership(user)
 		expect(TeamMembership.all.count).to eq(1)
 	end
 end
