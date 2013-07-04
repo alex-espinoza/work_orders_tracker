99b6b61b0c30c576ff6f17c0164f15c7d5122c6b
Alex Espinoza <aespinoza@aspria.net>
2013-07-03 21:14:05 -0400

    .gitignore now working

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-	:address							=> "smtp.gmail.com",
-	:port 								=> 587,
-	:domain								=> "gmail.com",
-	:user_name						=> "alex.espinoza.iphone@gmail.com",
-	:password							=> "Forallofthis90",
-	:authentication				=> "plain",
-	:enable_starttls_auto	=> true
-}
