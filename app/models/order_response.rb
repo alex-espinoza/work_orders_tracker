39efa1b460f63bb2702075336996384404805e6e
Alex Espinoza <aespinoza@aspria.net>
2013-07-05 19:41:34 -0400

    File attachment background jobs set up with Sidekiq

--- app/models/order_response.rb
+++ app/models/order_response.rb
@@ -1,7 +1,10 @@
+require 'file_size_validator'
+
 class OrderResponse < ActiveRecord::Base
   attr_accessible :file_attachment, :response, :order_id, :user_id
 
   mount_uploader :file_attachment, AttachmentUploader
+  process_in_background :file_attachment
 
   belongs_to :order,
   	inverse_of: :order_responses
@@ -13,4 +16,8 @@ class OrderResponse < ActiveRecord::Base
   validates_presence_of :order_id
   validates_presence_of :user_id
   validates_length_of :response, :minimum => 4, :maximum => 400
+  validates :file_attachment,
+  :file_size => {
+    :maximum => 5.megabytes.to_i
+  }
 end
