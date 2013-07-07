15443946944025b737b29a7b33370f70d0862896
Alex Espinoza <aespinoza@aspria.net>
2013-07-07 18:25:12 -0400

    Fixed carrierwave store_dir to work again with S3

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -9,6 +9,10 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   process :set_content_type
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   version :thumb do
     process :resize_to_fit => [250, 250]
   end
