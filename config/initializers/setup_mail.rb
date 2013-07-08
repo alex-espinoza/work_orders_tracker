ea99f942edd6d1b659eaebaef57cc2d654464dac
Alex Espinoza <aespinoza@aspria.net>
2013-07-07 20:46:43 -0400

    Changed ActionMailer settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-	:address							=> "smtp.gmail.com",
-	:port 								=> 587,
-	:domain								=> "gmail.com",
-	:user_name						=> "alex.espinoza.iphone@gmail.com",
-	:password							=> "Forallofthis90",
-	:authentication				=> "plain",
-	:enable_starttls_auto	=> true
-}
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
+ }
+
+ ActionMailer::Base.delivery_method = :smtp
